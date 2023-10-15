fe88f3631356c16de1a49724b07f61717ceae858
Garlic-Bread <ntshabangu208@gmail.com>
2023-10-15 22:53:24 +0200

    .

--- Sindiswa's Journey/Assets/Scenes/Owl's Realm/Scripts of LIFE/Bullets.cs
+++ Sindiswa's Journey/Assets/Scenes/Owl's Realm/Scripts of LIFE/Bullets.cs
@@ -4,54 +4,73 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
+    [SerializeField] private Collider2D colliderToIgnore;
     [SerializeField] private float followSpeed;
     [SerializeField] private float bulletLife;
     [SerializeField] private int bulletDamage;
     [SerializeField] private int damagePoint;
+    [SerializeField] private int bulletTypeNumber;
+
+    public string currentBulletTag;
+    public bool normalBullet = true;
+    public int layerNumberToIgnore;
+
     private GameObject player;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("PlayerFeet");
+        colliderToIgnore = gameObject.GetComponent<Collider2D>();
     }
     private void Update()
     {
-        if (this.gameObject.CompareTag("HomingBullet"))
-        {
-            BulletType(1);
-        }
+        BulletType(bulletTypeNumber);
 
-        else
-        {
-            Destroy(gameObject, bulletLife);
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.GetComponent<Health>() != null) //if what the bullet touches has health, deal damage to it
-        {
-            Health dealDamage = collision.gameObject.GetComponent<Health>();
-            dealDamage.Damage(bulletDamage);
+        Destroy(gameObject, bulletLife);
 
-            //increase the player's points when their bullet hits an enemy
-            if(collision.gameObject.layer == 3)
-            {
-                GameCurrency addPoint = FindObjectOfType<GameCurrency>();
-                addPoint.EarnPoints(damagePoint);
-            }
-        }
-        Destroy(gameObject);
     }
 
     // types of bullets
     private void BulletType(int bulletType)
     {
-        if (bulletType == 1) //homing bullet
+        if (bulletType == 1) //normal bullet
+        {
+            normalBullet = true;
+        }
+
+        else if (bulletType == 2) //homing bullet
         {
             print("homin on player");
+            normalBullet = false;
             Vector3 targetPos = player.transform.position;
             float speed = followSpeed * Time.deltaTime;
             transform.position = Vector3.Lerp(transform.position, targetPos, speed);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Health>() != null && normalBullet == true) //if what the bullet touches has health, deal damage to it
+        {
+            if (this.gameObject.CompareTag(currentBulletTag) && collision.gameObject.layer == layerNumberToIgnore)
+            {
+                print("jjj");
+                this.gameObject.GetComponent<Collider2D>().isTrigger = true;
+            }
+            else
+            {
+                Health dealDamage = collision.gameObject.GetComponent<Health>();
+                dealDamage.Damage(bulletDamage);
+                Destroy(gameObject);
+            
+                if (collision.gameObject.layer == 3) //increase the player's points when their bullet hits an enemy
+                {
+                    GameCurrency addPoint = FindObjectOfType<GameCurrency>();
+                    addPoint.EarnPoints(damagePoint);
+                }
+            }
+        }
+    }
+    
+
 }
--- Sindiswa's Journey/Assets/Scenes/Owl's Realm/Scripts of LIFE/Character_Controller.cs
+++ Sindiswa's Journey/Assets/Scenes/Owl's Realm/Scripts of LIFE/Character_Controller.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Character_Controller : MonoBehaviour
 {
     private Rigidbody2D rb;
-
+    [SerializeField] private float playerOriginalSpeed;
+    [SerializeField] private float bulletSpeed = 10f; // Speed of the bullet
+    [SerializeField] private float lockOnRange = 10f; // Range within which enemies can be locked onto
+    [SerializeField] private float fireRate = 0.5f; // Rate of fire (in seconds)
+    [SerializeField] private float nextFireTime; // Time of the next allowed fire
     [SerializeField] private float playerSpeed;
+
+    [SerializeField] private Transform playerGun; // Reference to the player's gun transform
+    [SerializeField] private LayerMask targetLayer;
+    [SerializeField] private GameObject bulletPrefab; // Prefab of the bullet to be spawned
     [SerializeField] private Joystick_Movement joystick;
 
     void Start()
@@ -14,9 +23,9 @@ public class Character_Controller : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void PlayerMove()
     {
-        if(joystick.joystick_Vector.y != 0)
+        if (joystick.joystick_Vector.y != 0)
         {
             rb.velocity = new Vector2(joystick.joystick_Vector.x * playerSpeed, joystick.joystick_Vector.y * playerSpeed);
         }
@@ -24,5 +33,88 @@ public class Character_Controller : MonoBehaviour
         {
             rb.velocity = Vector2.zero;
         }
+
+        //run to the right
+        if (joystick.joystick_Vector.x > 0.35f) 
+        {
+            rb.velocity = new Vector2(joystick.joystick_Vector.x * playerSpeed, joystick.joystick_Vector.y * playerSpeed);
+            //animator.SetBool("rightRun", true);
+        }
+        else if (joystick.joystick_Vector.x <= 0.35f)
+        {
+            //animator.SetBool("rightRun", false);
+        }
+
+        if (joystick.joystick_Vector.x < -0.35f) //run to the left
+        {
+            rb.velocity = new Vector2(joystick.joystick_Vector.x * playerSpeed, joystick.joystick_Vector.y * playerSpeed);
+            //animator.SetBool("leftRun", true);
+        }
+        else if (joystick.joystick_Vector.x >= -0.35f)
+        {
+            //animator.SetBool("leftRun", false);
+        }
+    }
+    void FireBullet()
+    {
+        // Instantiate a bullet prefab at the player's gun position and rotation
+        GameObject bullet = Instantiate(bulletPrefab, playerGun.position, Quaternion.identity);
+
+        // Apply velocity to the bullet in the forward direction of the gun
+        Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletRigidbody.velocity = playerGun.forward * bulletSpeed;
+    }
+    void FindEnemiesInScene() //lock on to enemies and shoot at them
+    {
+        GameObject[] enemies = FindObjectsOfType<GameObject>().Where(go => ((3 << go.layer) & targetLayer.value) != 0).ToArray();
+        if (enemies.Length > 0)
+        {
+            // Find the closest enemy
+            Transform closestEnemy = GetClosestEnemy(enemies);
+
+            // Check if the closest enemy is within lock-on range
+            if (Vector3.Distance(transform.position, closestEnemy.position) <= lockOnRange)
+            {
+                // Rotate the player's gun to face the closest enemy
+                Vector3 targetDirection = closestEnemy.position - playerGun.position;
+                Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+                playerGun.rotation = Quaternion.Lerp(playerGun.rotation, targetRotation, Time.deltaTime * 10f);
+
+                // Check if enough time has passed since the last fire
+
+                if (Time.time >= nextFireTime)
+                {
+                    // Fire a bullet
+                    FireBullet();
+                    // Set the time for the next allowed fire
+                    nextFireTime = Time.time + fireRate;
+                }
+            }
+        }
+    }
+
+    Transform GetClosestEnemy(GameObject[] enemies) //find enemies that are within range and lock on to them
+    {
+        Transform closestEnemy = null;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestEnemy = enemy.transform;
+            }
+        }
+
+        return closestEnemy;
+    } 
+
+    void FixedUpdate()
+    {
+        PlayerMove();
+        FindEnemiesInScene();
     }
 }
