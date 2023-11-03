fbed5ebceed3c35e5a93121f12adda0e3a05a99c
Musawenkosi <2127558@students.wits.ac.za>
2023-11-03 04:04:55 +0200

    moving camera is complete

---
+++ Sindiswa's Journey/Assets/Scenes/Big Moooose/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float moveSpeed = 5f;
+    public float pauseDuration = 2f;
+
+    private int currentWaypointIndex = 0;
+    private Vector3 targetPosition;
+    private bool isMoving = true;
+
+    private void Start()
+    {
+        SetTargetPosition();
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            MoveTowardsTarget();
+        }
+    }
+
+    private void SetTargetPosition()
+    {
+        targetPosition = waypoints[currentWaypointIndex].position;
+    }
+
+    private void MoveTowardsTarget()
+    {
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        transform.position = newPosition;
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+        {
+            StartCoroutine(PauseAtWaypoint());
+        }
+    }
+
+    private IEnumerator PauseAtWaypoint()
+    {
+        isMoving = false;
+        yield return new WaitForSeconds(pauseDuration);
+        isMoving = true;
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+        SetTargetPosition();
+    }
+}
